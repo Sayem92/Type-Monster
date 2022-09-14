9ab0dcf53ba6d420bf9d672cc39b94011ca75a59
Sayem92 <sayemmiha123@gmail.com>
2022-09-14 22:51:07 +0600

    add mistakes count

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount = errorCount + 1
   }
 
   // check if given question text is equal to user typed text
