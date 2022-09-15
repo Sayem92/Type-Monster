133a8e132111095bf2a88a8e684616c3f1ca0ac9
Sayem92 <sayemmiha123@gmail.com>
2022-09-15 12:29:28 +0600

    add time problem

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -133,8 +133,8 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = parseInt((currentTime - startTime) / 1000);
 
 
-  document.getElementById("show-time").innerHTML = `${startTime  ? timeSpent.toFixed(0) : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime  ? timeSpent : 0} seconds`;
 }, 1000);
